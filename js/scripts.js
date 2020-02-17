9bb93a24bb6c54801f20f80647efc23795d9878c
joseluislo12 <joseluislo12@hotmail.com>
2020-02-17 12:32:20 -0500

    Education Section
    Signed-off-by: joseluislo12 <joseluislo12@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -26,16 +26,16 @@
 function gotoPage(page) {
     switch(page){
         case 1:
-            $('<a href="mailto:j.lobolara@gmail.com" target="blank"></a>')[0].click();
+            $('<a href="mailto:j.lobolara@gmail.com" target="_blank"></a>')[0].click();
         break;
         case 2:
-            $('<a href="https://www.linkedin.com/in/joseluislobo" target="blank"></a>')[0].click();
+            $('<a href="https://www.linkedin.com/in/joseluislobo" target="_blank"></a>')[0].click();
         break;
         case 3:
-            $('<a href="https://github.com/joseluislo12" target="blank"></a>')[0].click();
+            $('<a href="https://github.com/joseluislo12" target="_blank"></a>')[0].click();
         break;
         case 4:
-            $('<a href="./files/Resume Jose Lobo.pdf" download="Resume Jose Lobo" target="blank"></a>')[0].click();
+            $('<a href="./files/Resume Jose Lobo.pdf" download="Resume Jose Lobo" target="_blank"></a>')[0].click();
         break;
     }
 }
